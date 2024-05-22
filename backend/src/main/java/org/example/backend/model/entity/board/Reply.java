cef6c8946b00587973b420cf9404fc658fef7c29
michealMS <chmstank912@gmail.com>
2024-05-22 16:09:52 +0900

    update_reply
    update_reply

--- backend/src/main/java/org/example/backend/model/entity/board/Reply.java
+++ backend/src/main/java/org/example/backend/model/entity/board/Reply.java
@@ -9,7 +9,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.example.backend.model.common.BaseTimeEntity2;
-import org.example.backend.model.common.ReplyId;
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.Where;
 
@@ -29,19 +28,22 @@ import java.util.UUID;
 @AllArgsConstructor
 @Where(clause = "STATUS = 'Y'")
 @SQLDelete(sql = "UPDATE TB_REPLY SET STATUS = 'N', DEL_DATE = TO_CHAR(SYSDATE, 'YYYY-MM-DD HH24:MI:SS') WHERE REPLY_ID = ?")
-@IdClass(ReplyId.class)
 public class Reply extends BaseTimeEntity2 {
     @Id
 //    @GeneratedValue(strategy = GenerationType.SEQUENCE
 //            , generator = "SEQ_TB_COUPON_CP_ID_GENERATOR"
 //    )
-    private Long replyId;       // 댓글 pk
 
-    @Id
-    private String uuid = UUID.randomUUID().toString();        // 파일ID // 무작위 UUID를 생성후 UUID를 문자열로 반환
+//re_reply	number
+//uuid	varchar2(1000 byte)
+//nickname	varchar2(100 byte)
+//reply	varchar2(4000 byte)
+//code	varchar2(255 byte)
+    private Long replyId;       // 댓글 pk
     private Long boardId;       // 글번호 fk
     private String memberId;    // 댓글작성자 fk
     private Integer reReply;    // 대댓글
+    private String uuid = UUID.randomUUID().toString();        // 파일ID // 무작위 UUID를 생성후 UUID를 문자열로 반환
     private String nickname;    // 별명
     private String reply;       // 내용
     private String code;        // 분류코드
