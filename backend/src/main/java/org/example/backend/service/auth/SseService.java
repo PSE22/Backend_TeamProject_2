666b05503dbd7af229b2fdcc1e07c19c6271e6ed
KimTaewan <158536479+naughtycoder95@users.noreply.github.com>
2024-05-25 01:43:31 +0900

    SSE Early Disconnected 이슈 해결
    TimeOut 1시간으로 설정했으나 20분 내외에서 Disconnected 되는 이슈 발생.
    하트비트 로직 추가 및 SSE 구독 메소드 트랜잭션 처리 후 테스트 결과 1시간 연결지속 후 타임아웃 정상작동 확인 완료.

--- backend/src/main/java/org/example/backend/service/auth/SseService.java
+++ backend/src/main/java/org/example/backend/service/auth/SseService.java
@@ -2,13 +2,17 @@ package org.example.backend.service.auth;
 
 import lombok.extern.slf4j.Slf4j;
 import org.example.backend.model.dto.NotifyDto;
-import org.example.backend.model.entity.Notify;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
 
 import java.io.IOException;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.TimeUnit;
 
 /**
  * packageName : org.example.backend.service.auth
@@ -28,31 +32,55 @@ import java.util.concurrent.ConcurrentHashMap;
 public class SseService {
 
     private final Map<String, SseEmitter> sseEmitters = new ConcurrentHashMap<>();
+    private final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
 
+    @Transactional
     void sendSseEvent(NotifyDto notifyDto) {
         SseEmitter emitter = sseEmitters.get(notifyDto.getMemberId());
         if (emitter != null) {
             try {
                 emitter.send(SseEmitter.event().name("notification").data(notifyDto));
             } catch (IOException e) {
-                sseEmitters.remove(notifyDto.getMemberId());
+                emitter.completeWithError(e);
+                sseEmitters.remove(notifyDto.getMemberId(), emitter);
             }
         }
     }
 
+    @Transactional
     public SseEmitter subscribe(SseEmitter emitter, String memberId) {
         sseEmitters.put(memberId, emitter);
-        System.out.println("new emitter added: " + emitter);
-        System.out.println("emitter list size: " + sseEmitters.size());
-        emitter.onCompletion(() -> {
-            System.out.println("onCompletion callback");
+        log.info("새로운 에미터 등록 {}", emitter);
+        log.info("등록된 에미터 수 {}", sseEmitters.size());
+
+        emitter.onError((error) -> {
+            log.error("onError callback", error);
+            emitter.completeWithError(error);
             sseEmitters.remove(memberId, emitter);
         });
-        emitter.onTimeout(() -> {
-            System.out.println("onTimeout callback");
-            emitter.complete();
+
+        emitter.onCompletion(() -> {
+            log.info("onCompletion callback");
+            sseEmitters.remove(memberId, emitter);
         });
 
+        emitter.onTimeout(() -> {
+            log.info("onTimeout callback");
+            emitter.complete();
+            sseEmitters.remove(memberId, emitter);
+        });
+
+        scheduler.scheduleAtFixedRate(() -> sendPing(emitter), 0, 1, TimeUnit.MINUTES);
+
         return emitter;
+    }
+
+    private void sendPing(SseEmitter emitter) {
+        try {
+            emitter.send("ping");
+            log.info("핑 {}", emitter);
+        } catch (Exception e) {
+            log.error("Error sending ping", e);
+        }
     }
 }
